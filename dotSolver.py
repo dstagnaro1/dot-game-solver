cc5133bf63de8bbb48812adfc8c4bd3ebcc506f2
dstagnaro1 <danielstagnaro@gmail.com>
2023-02-09 22:12:28 -0800

    fixed return_data_neighbors and worked on fill_02

--- dotSolver.py
+++ dotSolver.py
@@ -19,6 +19,11 @@ class Matrix:
 				return True
 		return False
 
+	def print_answers(self):
+		print()
+		for row in self.answers:
+			print(row)
+
 	def return_data_neighbors(self, row, col):
 		# taking the row and column in the matrix, return
 		# locations of the neighbors in the matrix as a list of 
@@ -33,33 +38,38 @@ class Matrix:
 
 		# above neighbor:
 		if col > 0:
-			neighbors.append([row-1, col])
+			neighbors.append([row, col-1])
 
 		# right neighbor
 		if row < len(self.data[0])-1:
-			neighbors.append([row, col+1])
+			neighbors.append([row+1, col])
 		
 		# below neighbor:
 		if col < len(self.data[0])-1:
-			neighbors.append([row+1, col])
+			neighbors.append([row, col+1])
 
 		# left neighbor
 		if row > 0:
-			neighbors.append([row, col-1])
+			neighbors.append([row-1, col])
 
 		return neighbors
 
 	def return_data_corners(self, row, col):
 		pass
 
-	def return_answer_neighbors(self):
-		pass
+	def return_answer_neighbors(self, row, col):
+		neighbors = []
+		neighbors.append([row, col])
+		neighbors.append([row, col+1])
+		neighbors.append([row+1, col+1])
+		neighbors.append([row+1, col])
+		return neighbors
 
-	def fill_0_and_4(self):
+	def fill_04(self):
 		# fill all 0's with 0
 		# fill all 4's with 1
 
-		print("\n\n")
+		print("")
 		for row in range(len(self.data[0])):
 			for col in range(len(self.data[0])):
 				#print(matrix[row][col])
@@ -74,8 +84,9 @@ class Matrix:
 					self.answers[row+1][col+1] = 1
 					self.answers[row][col+1] = 1
 
-		for row in self.answers:
-			print(row)
+		self.print_answers()
+		# for row in self.answers:
+		# 	print(row)
 
 	def fill_02(self):
 		# any 0-2 put two 1's on far side
@@ -83,19 +94,37 @@ class Matrix:
 		#neighbrs = box.return_data_neighbors(2,1)
 		#for indx in neighbrs:
 		#	print(box.data[indx[0]][indx[1]])
+						
+		
+		# find 0
+		# find neighbors of 0
+		# for all neighbors
+		# if neighbor == 2
+		#	in neighbor position
+		# place two dots on far side of 0
 
 		for row in range(len(self.data[0])):
 			for col in range(len(self.data[0])):
 				if self.data[row][col] == 0:
-					print(f"Found a 0, {row}, {col}")
+					# print(f"Found a 0, {row}, {col}")
 					neighbors = self.return_data_neighbors(row, col)
 					# neighbors = [[0,2],[1,3],[2,2],[1,1]]
 					for index in neighbors:
 						
-						print(index) #[0, 2]
+						# print(index) #[0, 2]
 						
 						if self.data[index[0]][index[1]] == 2:
-							print("Found a 2 ", end="")
+
+							# print(f"Found a 2 at {index[0]}{index[1]}")
+							# print(f"Found a 2 ", end="")
+							# find answers for where 2 is
+							neighbors = self.return_answer_neighbors(index[0],index[1])
+							for neighbor in neighbors:
+								if self.answers[neighbor[0]][neighbor[1]] == -1:
+									self.answers[neighbor[0]][neighbor[1]] = 1
+
+							# print(answers)
+							# for all answers, if answer is -1, set to 1
 
 							if (row-1 == index[0]) and (col == index[1]):
 								print("above")
@@ -105,17 +134,9 @@ class Matrix:
 								print("below")
 							elif (row == index[0]) and (col-1 == index[1]):
 								print("to the left")
-							
-		
-		# find 0
-		# find neighbors of 0
-		# for all neighbors
-		# if neighbor == 2
-		#	in neighbor position
-		# place two dots on far side of 0
 
-		
-
+		self.print_answers()
+	
 	def fill_13(self):
 		pass
 
@@ -123,14 +144,13 @@ class Matrix:
 		pass
 
 	def solve(self):
-		self.fill_0_and_4()
+		self.fill_04()
 		self.fill_02()
 		self.fill_13()
 		self.fill_03()
 
 	# any 1-3 put two 1's on far side
 	# any 0 -> 3 put three 3's on far side
-
 
 	# repeat until no changes
 	# any square that is complete now, mark with 0
@@ -214,29 +234,17 @@ while not correct_input:
 '''
 
 box = test_inputs[ri(0,len(test_inputs)-1)]
-# box = test_inputs[1]
+box = test_inputs[1]
 
 box = generate_Matrix(box)
 box.solve()
 # box.fill_0_and_4()
 
-neighbrs = box.return_data_neighbors(1,2)
-for indx in neighbrs:
-	print(box.data[indx[0]][indx[1]])
+# neighbrs = box.return_data_neighbors(1,2)
+# for indx in neighbrs:
+# 	print(box.data[indx[0]][indx[1]])
 
-print(box.check_if_complete())
+# print(box.check_if_complete())
 
 # output = format_input(box)
 # print(output[0], output[1])
-
-
-
-
-
-
-
-
-
-
-
-
